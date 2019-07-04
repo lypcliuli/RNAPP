e9a6500ea086b8b4e9a40ab3c58a2798f893ae39
lypcliuli <lypcliuli@163.com>
2019-07-04 22:57:16 +0800

    初始化

--- ZhangFeiApp/ios/ZFSubViewController.m
+++ ZhangFeiApp/ios/ZFSubViewController.m
@@ -15,7 +15,7 @@
 #import <React/RCTRootView.h>
 
 //引入SplashScreen库
-#import "RNSplashScreen.h"
+//#import "RNSplashScreen.h"
 
 @interface ZFSubViewController ()
 
@@ -35,7 +35,7 @@
     rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
     self.view = rootView;
     //运行SplashScreen库
-    [RNSplashScreen show];
+//    [RNSplashScreen show];
     // Do any additional setup after loading the view.
 }
 
